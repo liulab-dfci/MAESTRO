fb9e6cfb550a4521b564761bd0398a1aa65b74f5
chenfeiwang <>
2019-11-11 10:14:51 -0500

    MAESTRO release v1.0.0

--- MAESTRO/R/MAESTRO_install.R
+++ MAESTRO/R/MAESTRO_install.R
@@ -3,4 +3,5 @@ library(devtools)
 
 install_github("hms-dbmi/pagoda2", upgrade = "never")
 install_github("SUwonglab/scABC@v0.1", upgrade = "never")
+# install.packages("stringi", repos="https://mirrors.tongji.edu.cn/CRAN/")
 install_github("chenfeiwang/MAESTRO", upgrade = "never")
--- MAESTRO/R/scRNAseq_qc.R
+++ MAESTRO/R/scRNAseq_qc.R
@@ -1,20 +1,65 @@
 library(MAESTRO)
-argue = commandArgs(T)
+library(optparse)
 
-setwd(argue[9])
+option_list = list(
+  make_option(c("--prefix"), type = "character", default = "MAESTRO",
+              action = "store", help = "The prefix of the output files."
+  ),
+  make_option(c("--rseqc"), type = "logical", default = FALSE,
+              action = "store", help = "Do RSeQC or not."
+  ),
+  make_option(c("--outdir"), type = "character", default = FALSE,
+              action = "store", help = "The directory where the output files are stored."
+  ),
+  make_option(c("--platform"), type = "character", default = FALSE,
+              action = "store", help = "The platform of scRNA-seq."
+  ),
+  make_option(c("--bamstat"), type = "character", default = FALSE,
+              action = "store", help = "The result of bam_stat."
+  ),
+  make_option(c("--readdistr"), type = "character", default = FALSE,
+              action = "store", help = "The result of read_distribution."
+  ),
+  make_option(c("--qual"), type = "character", default = FALSE,
+              action = "store", help = "The result of read_quality."
+  ),
+  make_option(c("--nvc"), type = "character", default = FALSE,
+              action = "store", help = "The result of read_NVC."
+  ),
+  make_option(c("--gc"), type = "character", default = FALSE,
+              action = "store", help = "The result of read_GC."
+  ),
+  make_option(c("--genecov"), type = "character", default = FALSE,
+              action = "store", help = "The result of geneBody_coverage."
+  ),
+  make_option(c("--filtering"), type = "character", default = FALSE,
+              action = "store", help = "The result of scRNA_qc."
+  )
+)
+argue = parse_args(OptionParser(option_list = option_list, usage = "Generate QC plots."))
 
-bamstat_file = argue[1]
-readdistr_file = argue[2]
-qualcode_file = argue[3]
-nvc_file = argue[4]
-gc_file = argue[5]
-genecov_file = argue[6]
-countgene_file = argue[7]
-prefix = argue[8]
+setwd(argue$outdir)
+bamstat_file = argue$bamstat
+readdistr_file = argue$readdistr
+qualcode_file = argue$qual
+nvc_file = argue$nvc
+gc_file = argue$gc
+genecov_file = argue$genecov
+countgene_file = argue$filtering
+prefix = argue$prefix
+rseqc = argue$rseqc
+platform = argue$platform
 
-RNAReadDistrPlot(bamstat.filepath = bamstat_file, readdistr.filepath = readdistr_file, name = prefix)
-RNAReadQualityPlot(filepath = qualcode_file, name = prefix)
-RNANucleotidePlot(filepath = nvc_file, name = prefix)
-RNAGCcontentPlot(filepath = gc_file, name = prefix)
-RNAGenebodyCoveragePlot(filepath = genecov_file, name = prefix)
-RNAFilteringPlot(filepath = countgene_file, UMI.cutoff = 1000, gene.number.cutoff = 500, name = prefix)
+if(rseqc){
+  RNAReadDistrPlot(bamstat.filepath = bamstat_file, readdistr.filepath = readdistr_file, name = prefix, platform = platform)
+  RNAReadQualityPlot(filepath = qualcode_file, name = prefix)
+  RNANucleotidePlot(filepath = nvc_file, name = prefix)
+  RNAGCcontentPlot(filepath = gc_file, name = prefix)
+  RNAGenebodyCoveragePlot(filepath = genecov_file, name = prefix)
+  RNAFilteringPlot(filepath = countgene_file, UMI.cutoff = 1000, gene.number.cutoff = 500, name = prefix)
+}else if(platform == "10x-genomics"){
+  RNAReadDistrPlot(bamstat.filepath = bamstat_file, readdistr.filepath = readdistr_file, name = prefix, platform = platform)
+  RNAFilteringPlot(filepath = countgene_file, UMI.cutoff = 1000, gene.number.cutoff = 500, name = prefix)
+}else{
+  RNAFilteringPlot(filepath = countgene_file, UMI.cutoff = 1000, gene.number.cutoff = 500, name = prefix)
+}
