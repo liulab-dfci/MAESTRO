4be4add7e8300b61c303a6ceaf38129606aac49f
DongqingSun96 <48665363+DongqingSun96@users.noreply.github.com>
2020-01-07 10:43:53 +0800

    remove scABC

--- MAESTRO/R/MAESTRO_install.R
+++ MAESTRO/R/MAESTRO_install.R
@@ -1,7 +1,7 @@
 # install required R packages
 library(devtools)
 
-install_github("hms-dbmi/pagoda2", upgrade = "never")
+# install_github("hms-dbmi/pagoda2", upgrade = "never")
 install_github("SUwonglab/scABC@v0.1", upgrade = "never")
 install_version(package = "Seurat", version = "3.0.2", repos="https://mirrors.tongji.edu.cn/CRAN/")
 install_github("chenfeiwang/MAESTRO", upgrade = "never")
