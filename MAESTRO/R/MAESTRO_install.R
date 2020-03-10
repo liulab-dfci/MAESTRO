600a65897c2e6b50581d58a1cfae9099ec9e0549
DongqingSun96 <48665363+DongqingSun96@users.noreply.github.com>
2020-03-10 19:00:21 +0800

    Update MAESTRO_install.R

--- MAESTRO/R/MAESTRO_install.R
+++ MAESTRO/R/MAESTRO_install.R
@@ -4,5 +4,6 @@ library(devtools)
 install_github("hms-dbmi/pagoda2", upgrade = "never")
 install_github("SUwonglab/scABC@v0.1", upgrade = "never")
 install_github("GreenleafLab/chromVARmotifs")
-# install_version(package = "Seurat", version = "3.0.2", repos="https://mirrors.tongji.edu.cn/CRAN/")
 install_github("chenfeiwang/MAESTRO", upgrade = "never")
+# install_version(package = "Seurat", version = "3.0.2", repos="https://mirrors.tongji.edu.cn/CRAN/")
+
