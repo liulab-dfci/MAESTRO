9d8a888d649555fc8242d0cb9ec43d0874d8e8b4
Dongqing Sun <dongqingsun96@gmail.com>
2020-05-07 17:32:21 +0800

    Add score in RNAAnnotateCelltype

--- R/RNAAnnotateCelltype.R
+++ R/RNAAnnotateCelltype.R
@@ -46,23 +46,31 @@ RNAAnnotateCelltype <- function(RNA, genes, signatures = "human.immune.CIBERSORT
                           return(toupper(as.character(signatures[which(signatures[,1]==celltypes[x]),2])))})
         names(signature_list) <- celltypes
             
-        cluster_celltypes = sapply(as.integer(unique(RNA@meta.data$seurat_clusters))-1, function(x){
-        idx = genes$cluster==x
-        avglogFC = genes$avg_logFC[idx]
-        names(avglogFC) = toupper(genes$gene[idx])
-        score_cluster = sapply(signature_list, function(y){
-                        score = sum(avglogFC[y], na.rm = TRUE) / log2(length(y))
-                        return(score)
+        cluster_celltype_score = sapply(as.integer(unique(RNA@meta.data$seurat_clusters))-1, function(x){
+            idx = genes$cluster==x
+            avglogFC = genes$avg_logFC[idx]
+            names(avglogFC) = toupper(genes$gene[idx])
+            score_cluster = sapply(signature_list, function(y){
+                score = sum(avglogFC[y], na.rm = TRUE) / log2(length(y))
+                return(score)
+            })
         })
-        if(max(score_cluster, na.rm = TRUE)>min.score)
-          cluster_celltypes = names(score_cluster)[which.max(score_cluster)]
-        else
-          cluster_celltypes = "Others"
+        colnames(cluster_celltype_score) = as.character(as.integer(unique(RNA@meta.data$seurat_clusters))-1)
+        
+        cellscore_max = apply(cluster_celltype_score, 2, max, na.rm = TRUE)
+        cellscore_max_celltype = apply(cluster_celltype_score, 2, function(x){
+            if (max(x) < min.score){
+                return("Others")
+            }else{
+                return(rownames(cluster_celltype_score)[which.max(x)])
+            }
         })
         
-        current.cluster.ids = as.integer(unique(RNA@meta.data$seurat_clusters))-1
-        new.cluster.ids = cluster_celltypes
-        RNA@meta.data$assign.ident = Idents(RNA)[rownames(RNA@meta.data)]
+        RNA@meta.data$assign.ident = as.character(as.integer(RNA@meta.data$seurat_clusters)-1)
+        current.cluster.ids = as.character(as.integer(unique(RNA@meta.data$seurat_clusters))-1)
+        new.cluster.ids = cellscore_max_celltype
+        
+        RNA@meta.data$assign.score = cellscore_max[RNA@meta.data$assign.ident]
         RNA@meta.data$assign.ident = plyr::mapvalues(x = RNA@meta.data$assign.ident,
                                                            from = current.cluster.ids, to = new.cluster.ids)
         if(signatures_name == "human.immune.CIBERSORT") {
