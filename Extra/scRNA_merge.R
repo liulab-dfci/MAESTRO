0c69127f26d605ac7a5de3935c1f2da86fdd073a
Tao Liu <vladimir.liu@gmail.com>
2020-05-29 14:23:32 -0400

    update two more scripts for plotting

--- Extra/scRNA_merge.R
+++ Extra/scRNA_merge.R
@@ -1,11 +1,15 @@
 #!/usr/bin/env Rscript
 
+# This script is intended to be an executable script
+# Work is needed to make it into R methods
+
 # example to merge three conditions
 MAESTRO.rdsfiles <- c("1_scRNA_Object.rds",  "2_scRNA_Object.rds", "3_scRNA_Object.rds") 
 MAESTRO.labels <- c("1","2","3")
 
 # configuration
 project.name <- "scRNA.merged"
+organism <- "GRCh38"
 dim.use <- 1:15
 cluster.res <- 0.6
 min.c <- 10
@@ -15,28 +19,31 @@ genes.cutoff <- 1e-5
 genes.pct <- 0.1
 genes.logfc <- 0.25
 only.pos <- FALSE
+condadir <- "/mnt/lustre/users/tliu/miniconda3"
+lisaenv <- "lisa"
 
 #=======code==========
 library(MAESTRO)
 library(Seurat)
 library(Gmisc)
+library(purrr)
 
 META <- data.frame( labels=MAESTRO.labels, rds=MAESTRO.rdsfiles )
 
-scRNA.objects <- c( readRDS(MAESTRO.rdsfiles[1]), readRDS(MAESTRO.rdsfiles[2]), readRDS(MAESTRO.rdsfiles[3]) )
+scRNA.objects <- purrr::map( MAESTRO.rdsfiles, readRDS )
 
-scRNA.merged <- merge(scRNA.objects[1]$RNA, y=c(scRNA.objects[2]$RNA, scRNA.objects[3]$RNA), add.cell.ids=META$labels, project=project.name)
+scRNA.merged <- merge(scRNA.objects[[1]]$RNA, y=purrr::map(scRNA.objects[2:length(scRNA.objects)],function(x){x$RNA}), add.cell.ids=MAESTRO.labels, project=project.name)
 
 # save the merged object
 saveRDS(scRNA.merged,file=paste0(project.name,".rds"))
 
 # tweak the parameters if necessary
-scRNA.merged.res <- RNARunSeurat(inputMat=GetAssayData(object = scRNA.merged, slot = 'counts'), project = project.name, min.c = min.c, min.g = min.g, mito = FALSE, variable.genes = 2000, organism = "GRCh38", dims.use = dim.use, cluster.res = cluster.res, only.pos = FALSE, genes.test.use = genes.test.use, genes.cutoff = genes.cutoff, genes.pct = genes.pct, genes.logfc = genes.logfc)
+scRNA.merged.res <- RNARunSeurat(inputMat=GetAssayData(object = scRNA.merged, slot = 'counts'), project = project.name, min.c = min.c, min.g = min.g, mito = FALSE, variable.genes = 2000, organism = organism, dims.use = dim.use, cluster.res = cluster.res, only.pos = FALSE, genes.test.use = genes.test.use, genes.cutoff = genes.cutoff, genes.pct = genes.pct, genes.logfc = genes.logfc)
 
 # fix the orig.ident as the new labels
-a <- scRNA.merged.res$RNA$orig.ident
+a <- scRNA.merged$orig.ident
 n <- names(a)
-t <- as.factor(gsub("_.*","",n))
+t <- as.factor(gsub("\d_.*","",n))
 t <- factor( t, levels=META$labels )
 names(t) <- n
 scRNA.merged.res$RNA$orig.ident <- t
@@ -72,3 +79,11 @@ scRNA.merged.res$genes <- cluster.genes
 
 # save result after removing cell cycle effects
 saveRDS(scRNA.merged.res,file=paste0(project.name,".res.after.cell.cycle.regression.rds"))
+
+#========average expression for each cluster=========
+cluster.averages <- AverageExpression(rna.aftercc$RNA)
+write.table(x=cluster.averages[["RNA"]][, ], file="cluster.average.expression.tsv")
+
+#=======Lisa analysis==========
+scRNA.merged.tfs <- RNAAnnotateTranscriptionFactor(RNA = scRNA.merged.res$RNA, genes = scRNA.merged.res$genes, project = project.name, method = "LISA", lisa.mode = "local", conda.dir = condadir, lisa.envname = lisaenv, organism = organism, top.tf = 10)
+saveRDS(scRNA.merged.tfs, file = paste0(project.name, ".tfs.rds"))
