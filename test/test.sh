08f32bcacd54f08f454b5386baa1a1ee8754da96
Tao Liu <vladimir.liu@gmail.com>
2020-07-19 01:25:03 -0400

    add more info in test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,7 @@
 
 TEST_PREFIX=test
 
+echo "1. test support of gzipped file of scATAC_10x_PeakCount.py"
 
 # testing support of gzipped file of scATAC_10x_PeakCount.py
 
@@ -10,3 +11,8 @@ TEST_BC_GZ=test.barcode.txt.gz
 TEST_FRAG_GZ=test.fragments.tsv.gz
 
 MAESTRO scatac-peakcount --peak $TEST_PEAK_GZ --fragment $TEST_FRAG_GZ --barcode $TEST_BC_GZ --species GRCh38 --cores 1 --outprefix $TEST_PREFIX
+ls -l $TEST_PREFIX
+
+#
+
+echo "Done!"
