4088fe8cf78903483f47a291ee408fcc652c9ac9
Gali Bai <gali.bai@hotmail.com>
2020-11-24 03:12:36 -0500

    fixed merge_h5 bugs

--- MAESTRO/scATAC_H5Process.py
+++ MAESTRO/scATAC_H5Process.py
@@ -24,31 +24,31 @@ def mtxtoh5_parser(subparsers):
     Add main function mtx-to-h5 argument parsers.
     """
 
-    workflow = subparsers.add_parser("mtx-to-h5", 
+    workflow = subparsers.add_parser("mtx-to-h5",
         help = "Convert 10X mtx format matrix to HDF5 format.")
     group_input = workflow.add_argument_group("Input files arguments")
-    group_input.add_argument("--type", dest = "datatype", default = "Peak", 
-        choices = ["Peak", "Gene"], 
+    group_input.add_argument("--type", dest = "datatype", default = "Peak",
+        choices = ["Peak", "Gene"],
         help = "Type of the count matrix (Peak for scATAC-seq and Gene for scRNA-seq). DEFAULT: Peak.")
-    group_input.add_argument("--matrix", dest = "matrix", default = "matrix.mtx", 
+    group_input.add_argument("--matrix", dest = "matrix", default = "matrix.mtx",
         help = "Location of .mtx formatted matrix file. DEFAULT: matrix.mtx.")
-    group_input.add_argument("--feature", dest = "feature", default = "features.tsv", 
+    group_input.add_argument("--feature", dest = "feature", default = "features.tsv",
         help = "Location of feature file (required for the format of 'mtx'). Features correspond to row indices of count matrix. "
         "If the type is Peak, please provide the peak bed file with 3 columns. "
         "If the type is Gene, each row should include gene name. DEFAULT: features.tsv.")
     group_input.add_argument("--gene-column", dest = "gene_column", default = 2, type = int,
         help = "If the type is 'Peak', please specify which column of the feature file to use for gene names. DEFAULT: 2.")
-    group_input.add_argument("--barcode", dest = "barcode", default = "barcodes.tsv", 
+    group_input.add_argument("--barcode", dest = "barcode", default = "barcodes.tsv",
         help = "Location of barcode file. Cell barcodes correspond to column indices of count matrix. DEFAULT: barcodes.tsv. ")
-    group_input.add_argument("--species", dest = "species", default = "GRCh38", 
-        choices = ["GRCh38", "GRCm38"], type = str, 
+    group_input.add_argument("--species", dest = "species", default = "GRCh38",
+        choices = ["GRCh38", "GRCm38"], type = str,
         help = "Species (GRCh38 for human and GRCm38 for mouse). DEFAULT: GRCh38.")
-    
+
 
     group_output = workflow.add_argument_group("Output arguments")
-    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO", 
+    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO",
         help = "Path to the directory where the result file shall be stored. DEFAULT: MAESTRO.")
-    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO", 
+    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO",
         help = "Prefix of output files. DEFAULT: MAESTRO.")
 
 def mtxtocount_parser(subparsers):
@@ -56,31 +56,31 @@ def mtxtocount_parser(subparsers):
     Add main function mtx-to-count argument parsers.
     """
 
-    workflow = subparsers.add_parser("mtx-to-count", 
+    workflow = subparsers.add_parser("mtx-to-count",
         help = "Convert 10X mtx format matrix to plain text count table.")
     group_input = workflow.add_argument_group("Input files arguments")
-    group_input.add_argument("--type", dest = "datatype", default = "Peak", 
-        choices = ["Peak", "Gene"], 
+    group_input.add_argument("--type", dest = "datatype", default = "Peak",
+        choices = ["Peak", "Gene"],
         help = "Type of the count matrix (Peak for scATAC-seq and Gene for scRNA-seq). DEFAULT: Peak.")
-    group_input.add_argument("--matrix", dest = "matrix", default = "matrix.mtx", 
+    group_input.add_argument("--matrix", dest = "matrix", default = "matrix.mtx",
         help = "Location of .mtx formatted matrix file. DEFAULT: matrix.mtx.")
-    group_input.add_argument("--feature", dest = "feature", default = "features.tsv", 
+    group_input.add_argument("--feature", dest = "feature", default = "features.tsv",
         help = "Location of feature file (required for the format of 'mtx'). Features correspond to row indices of count matrix. "
         "If the type is Peak, please provide the peak bed file with 3 columns. "
         "If the type is Gene, each row should include gene name. DEFAULT: features.tsv.")
     group_input.add_argument("--gene-column", dest = "gene_column", default = 2, type = int,
         help = "If the type is 'Peak', please specify which column of the feature file to use for gene names. DEFAULT: 2.")
-    group_input.add_argument("--barcode", dest = "barcode", default = "barcodes.tsv", 
+    group_input.add_argument("--barcode", dest = "barcode", default = "barcodes.tsv",
         help = "Location of barcode file. Cell barcodes correspond to column indices of count matrix. DEFAULT: barcodes.tsv. ")
-    group_input.add_argument("--species", dest = "species", default = "GRCh38", 
-        choices = ["GRCh38", "GRCm38"], type = str, 
+    group_input.add_argument("--species", dest = "species", default = "GRCh38",
+        choices = ["GRCh38", "GRCm38"], type = str,
         help = "Species (GRCh38 for human and GRCm38 for mouse). DEFAULT: GRCh38.")
-    
+
 
     group_output = workflow.add_argument_group("Output arguments")
-    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO", 
+    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO",
         help = "Path to the directory where the result file shall be stored. DEFAULT: MAESTRO.")
-    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO", 
+    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO",
         help = "Prefix of output files. DEFAULT: MAESTRO.")
 
 
@@ -89,29 +89,29 @@ def counttoh5_parser(subparsers):
     Add main function count-to-h5 argument parsers.
     """
 
-    workflow = subparsers.add_parser("count-to-h5", 
+    workflow = subparsers.add_parser("count-to-h5",
         help = "Convert plain text count table to HDF5 format.")
     group_input = workflow.add_argument_group("Input files arguments")
-    group_input.add_argument("--type", dest = "datatype", default = "Peak", 
-        choices = ["Peak", "Gene"], 
+    group_input.add_argument("--type", dest = "datatype", default = "Peak",
+        choices = ["Peak", "Gene"],
         help = "Type of the count matrix (Peak for scATAC-seq and Gene for scRNA-seq). DEFAULT: Peak.")
-    group_input.add_argument("--count", dest = "count", default = "", 
+    group_input.add_argument("--count", dest = "count", default = "",
         help = "Location of plain text count table file. "
         "If the type is Peak, the row name should be like 'chromosome_peakstart_peakend'. "
         "For example, 'chr10_100020591_100020841'.")
-    group_input.add_argument("--separator", dest = "separator", default = "tab", 
+    group_input.add_argument("--separator", dest = "separator", default = "tab",
         choices = ["tab", "space", "comma"],
         help = "The separating character (only for the format of 'plain'). "
         "Values on each line of the plain matrix file will be separated by the character. DEFAULT: tab.")
-    group_input.add_argument("--species", dest = "species", default = "GRCh38", 
-        choices = ["GRCh38", "GRCm38"], type = str, 
+    group_input.add_argument("--species", dest = "species", default = "GRCh38",
+        choices = ["GRCh38", "GRCm38"], type = str,
         help = "Species (GRCh38 for human and GRCm38 for mouse). DEFAULT: GRCh38.")
-    
+
 
     group_output = workflow.add_argument_group("Output arguments")
-    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO", 
+    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO",
         help = "Path to the directory where the result file shall be stored. DEFAULT: MAESTRO.")
-    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO", 
+    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO",
         help = "Prefix of output files. DEFAULT: MAESTRO.")
 
 
@@ -120,23 +120,23 @@ def h5tocount_parser(subparsers):
     Add main function h5-to-count argument parsers.
     """
 
-    workflow = subparsers.add_parser("h5-to-count", 
+    workflow = subparsers.add_parser("h5-to-count",
         help = "Convert HDF5 format to plain text count table.")
     group_input = workflow.add_argument_group("Input files arguments")
-    group_input.add_argument("--type", dest = "datatype", default = "Peak", 
-        choices = ["Peak", "Gene"], 
+    group_input.add_argument("--type", dest = "datatype", default = "Peak",
+        choices = ["Peak", "Gene"],
         help = "Type of the count matrix (Peak for scATAC-seq and Gene for scRNA-seq). DEFAULT: Peak.")
-    group_input.add_argument("--h5", dest = "h5", default = "", 
+    group_input.add_argument("--h5", dest = "h5", default = "",
         help = "Location of HDF5 format file. ")
-    group_input.add_argument("--species", dest = "species", default = "GRCh38", 
-        choices = ["GRCh38", "GRCm38"], type = str, 
+    group_input.add_argument("--species", dest = "species", default = "GRCh38",
+        choices = ["GRCh38", "GRCm38"], type = str,
         help = "Species (GRCh38 for human and GRCm38 for mouse). DEFAULT: GRCh38.")
-    
+
 
     group_output = workflow.add_argument_group("Output arguments")
-    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO", 
+    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO",
         help = "Path to the directory where the result file shall be stored. DEFAULT: MAESTRO.")
-    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO", 
+    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO",
         help = "Prefix of output files. DEFAULT: MAESTRO.")
 
 def mergeh5_parser(subparsers):
@@ -144,28 +144,28 @@ def mergeh5_parser(subparsers):
     Add main function merge-h5 argument parsers.
     """
 
-    workflow = subparsers.add_parser("merge-h5", 
+    workflow = subparsers.add_parser("merge-h5",
         help = "Merge 10X HDF5 files.")
     group_input = workflow.add_argument_group("Input files arguments")
-    group_input.add_argument("--type", dest = "datatype", default = "Peak", 
-        choices = ["Peak", "Gene"], 
+    group_input.add_argument("--type", dest = "datatype", default = "Peak",
+        choices = ["Peak", "Gene"],
         help = "Type of the count matrix (Peak for scATAC-seq and Gene for scRNA-seq). DEFAULT: Peak.")
     group_input.add_argument("--h5", dest = "h5_list", default = [], nargs = "+",
         help = "Location of HDF5 count files. Multiple files should be separated by space. "
         "For example, --h5 A.h5 B.h5 C.h5 ")
-    group_input.add_argument("--species", dest = "species", default = "GRCh38", 
-        choices = ["GRCh38", "GRCm38"], type = str, 
+    group_input.add_argument("--species", dest = "species", default = "GRCh38",
+        choices = ["GRCh38", "GRCm38"], type = str,
         help = "Species (GRCh38 for human and GRCm38 for mouse). DEFAULT: GRCh38.")
-    
+
 
     group_output = workflow.add_argument_group("Output arguments")
     group_output.add_argument("--cellprefix", dest = "cellprefix_list", default = [], nargs = "+",
         help = "Prefix to add to cell identities. Multiple prefixes should be separated by space "
         "and the number should be equal to that of HDF5 files. "
         "If not set, cell original cell identities will be kept.")
-    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO", 
+    group_output.add_argument("-d", "--directory", dest = "directory", default = "MAESTRO",
         help = "Path to the directory where the result file shall be stored. DEFAULT: MAESTRO.")
-    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO", 
+    group_output.add_argument("--outprefix", dest = "outprefix", default = "MAESTRO",
         help = "Prefix of output files. DEFAULT: MAESTRO.")
 
 
@@ -245,7 +245,8 @@ def merge_10X_h5(directory, outprefix, h5list, prefixlist, genome = 'GRCh38', da
     if_features_same = True
     for i in range(0, len(features_list)-1):
         if if_features_same:
-            if_features_same = if_features_same & (features_list[i] == features_list[i+1]).all()
+            #if_features_same = if_features_same & (features_list[i] == features_list[i+1]).all()
+            if_features_same = if_features_same & numpy.array_equal(features_list[i],features_list[i+1])
         else:
             break
 
@@ -291,7 +292,7 @@ def read_10X_mtx(matrix_file, feature_file, barcode_file, datatype, gene_column 
         feature_in = gzip.open(feature_file, "r")
     else:
         feature_in = open(feature_file, "r")
-    features = feature_in.readlines() 
+    features = feature_in.readlines()
     if datatype == "Peak":
         features = ["_".join(feature.strip().split("\t")[0:3]) for feature in features]
     else:
@@ -304,7 +305,7 @@ def read_10X_mtx(matrix_file, feature_file, barcode_file, datatype, gene_column 
         barcode_in = gzip.open(barcode_file, "r")
     else:
         barcode_in = open(barcode_file, "r")
-    barcodes = barcode_in.readlines() 
+    barcodes = barcode_in.readlines()
     if type(barcodes[0]) == str:
         barcodes = [barcode.strip().split("\t")[0] for barcode in barcodes]
     if type(barcodes[0]) == bytes:
@@ -330,7 +331,7 @@ def mtx_2_h5(directory, outprefix, matrix_file, feature_file, barcode_file, gene
 
     matrix_dict = read_10X_mtx(matrix_file = matrix_file, feature_file = feature_file, barcode_file = barcode_file, datatype = datatype, gene_column = gene_column)
 
-    write_10X_h5(filename = filename, matrix = matrix_dict["matrix"], 
+    write_10X_h5(filename = filename, matrix = matrix_dict["matrix"],
         features = matrix_dict["features"], barcodes = matrix_dict["barcodes"], genome = genome, datatype = datatype)
 
 
@@ -435,6 +436,5 @@ def count_2_h5(directory, outprefix, count_file, separator, genome = 'GRCh38', d
     matrix_dict = read_count(count_file, separator)
 
 
-    write_10X_h5(filename = filename, matrix = matrix_dict["matrix"], 
+    write_10X_h5(filename = filename, matrix = matrix_dict["matrix"],
         features = matrix_dict["features"], barcodes = matrix_dict["barcodes"], genome = genome, datatype = datatype)
-
